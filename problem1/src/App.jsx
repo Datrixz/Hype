53bbadc2a4afb953a95b8a3dedee4a02f55f113a
Datrixz <vsh.pranjal@gmail.com>
2022-05-11 10:41:10 +0530

    commit

---
+++ problem1/src/App.jsx
@@ -0,0 +1,91 @@
+import { useState } from "react";
+
+function App() {
+  const [input, setInput] = useState(""); //to store input box value
+  const [output, setOutput] = useState([]); //to store the character count in
+
+  // OnSubmit function for Form
+  const display = (e) => {
+    setOutput([]);                //initialize output as empty on each click
+    const charArr = [...input];  //Array of chracters from string
+    const count = {};           //Object to store {char : count} pair
+
+    // Iterating through "charArr" and storing count values in "count" object
+    charArr.map((element) => {
+      if (
+        (element >= "A" && element <= "Z") ||
+        (element >= "a" && element <= "z")
+      ) {
+        element = element.toUpperCase();
+
+        if (count[element]) {
+          count[element] += 1;
+        } else {
+          count[element] = 1;
+        }
+      }
+    });
+
+    // Seperating Keys and Values and storing in individual array
+    const keys = Object.keys(count);
+    const values = Object.values(count);
+
+    // storing key and values pair in seperate array for displaying data.
+    let temp = "";
+    let temArr = [];
+    for (let i = 0; i < keys.length; i++) {
+      temp = `${keys[i]}-${values[i]}`;
+      temArr = [...temArr, temp];
+      console.log(temArr);
+    }
+
+    //Finally setting the "output = temporary array"
+    setOutput(temArr);
+
+    //Prevent default behaviour of refreshing  page on submission
+    e.preventDefault();
+  };
+
+  return (
+    <div className="App">
+      <div className="container bg-gray-400 flex justify-center items-center min-h-screen w-screen max-w-screen p-6">
+        <div className="min-h-[80vh] max-h-[80vh] w-[80vw] bg-slate-100 rounded-3xl shadow-lg flex flex-col p-6">
+          <div className="text-4xl font-medium text-center">
+            <h1>Character Counter</h1>
+          </div>
+          <form onSubmit={display}>
+            <textarea
+              type="text"
+              name="input"
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              className="placeholder:text-slate-400 outline-none rounded-md my-8 text-xl outline outline-1 outline-stone-400 p-2 w-full resize-none"
+              rows={3}
+              placeholder="Type your text here..."
+            />
+            <br />
+            <button
+              type="submit"
+              className="bg-blue-400 px-4 py-2 rounded-lg drop-shadow-md text-white shadow-blue-400 font-bold hover:bg-blue-500 active:bg-blue-800 active:drop-shadow-none"
+            >
+              Submit
+            </button>
+          </form>
+          <hr className="my-6" />
+          <h2 className="text-3xl mb-6">Result:</h2>
+          <div id="output-area" className="flex-1 break-words overflow-y-auto">
+            {output.map((e, i) => {
+              return (
+                <div key={i}>
+                  <h1>{e}</h1>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ problem2/src/App.jsx
@@ -0,0 +1,86 @@
+import axios from "axios";
+import { useState } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(false); //Optional feature for loading spinner
+  const [names, setNames] = useState([]);
+
+  const add = () => {
+    setLoading(true);
+    let flag = true;
+    let random = 1;
+    while (flag) {
+      let inrandom = Math.floor(Math.random() * 50) + 1;
+      if(names.length===0){break}
+      for (let i = 0; i < names.length; i++) {
+        if (names[i].id === inrandom) {
+          flag = true;
+          break;
+        } else {
+          flag = false;
+        }
+      }
+      random=inrandom
+    }
+    let temp = {};
+    axios
+      .get(`https://swapi.dev/api/people/${random}`)
+      .then((response) => {
+        temp = { id: random, name: response.data.name };
+        setNames([...names, temp]);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const del = (e) => {
+    let delId = e.target.getAttribute("name");
+    console.log(delId);
+    const newArrTemp = names.filter((item) => {
+      return item.id != delId;
+    });
+    setNames(newArrTemp);
+  };
+
+  const loadScr = <div className="text-6xl pt-20">Loading...</div>;
+
+  const display = names.map((e) => {
+    return (
+      <div
+        key={e.id}
+        className="p-6 md:w-[40vw] rounded-md shadow-lg text-left text-lg font-semibold flex justify-between"
+      >
+        {e.name}
+        <button
+          name={e.id}
+          onClick={del}
+          className="outline-none border-none bg-red-500 rounded-lg shadow-md px-2 text-white"
+        >
+          delete
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <div className="App">
+      <div className="container h-screen w-screen flex flex-col items-center p-4">
+        <h1 className="text-4xl font-bold my-4">Add Star Wars Characters:</h1>
+        <button
+          className="outline-none border-none w-fit bg-blue-500 disabled:bg-gray-400 rounded-lg px-6 py-3 text-white font-bold"
+          onClick={add}
+          disabled={loading}
+        >
+          Add Record
+        </button>
+        {loading ? loadScr : display}
+      </div>
+    </div>
+  );
+}
+
+export default App;
